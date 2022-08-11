2be22ec454e2eeece60c12c62ce57363ccec8eb1
Jason Martinez <jasonmar08@gmail.com>
2022-08-11 11:51:49 -0400

    added written_posts associations and created written_posts seeder

--- seeders/20220811153017-written_posts.js
+++ seeders/20220811153017-written_posts.js
@@ -1,22 +1,31 @@
 'use strict'
 
+//// TEMPLATE:
+
+// {
+//   title: '',
+//   text: '',
+//   userId: ,
+//   createdAt: new Date(),
+//   updatedAt: new Date()
+// }
+
+const written_posts = [
+  {
+    title: 'Living my best life!',
+    text: 'Just started an apprenticeship at a startup called Tesla. Grateful and excited for this opportunity. #Blessed #ElonIsTheGoat',
+    userId: 1,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }
+]
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    return queryInterface.bulkInsert(
-      'written_posts',
-      [
-        {
-          title: '',
-          text: '',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        }
-      ],
-      {}
-    )
+    return queryInterface.bulkInsert('written_posts', written_posts)
   },
 
   async down(queryInterface, Sequelize) {
-    return queryInterface.bulkDelete('written_posts', null, {})
+    return queryInterface.bulkDelete('written_posts', written_posts)
   }
 }
