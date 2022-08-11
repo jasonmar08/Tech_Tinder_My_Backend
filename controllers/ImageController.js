92b5e323bb4ffd46e68b9a090a3055ae6c37cd58
MatthewScott <mattwillscott95@gmail.com>
2022-08-11 11:49:20 -0400

    set up video and image controllers and routers and tested them to make sure they all work

---
+++ controllers/ImageController.js
@@ -0,0 +1,51 @@
+const { Image_Post } = require('../models')
+
+const GetPosts = async (req, res) => {
+  try {
+    const posts = await Image_Post.findAll()
+    res.send(posts)
+  } catch (error) {
+    throw error
+  }
+}
+
+const CreatePost = async (req, res) => {
+  try {
+    const post = await Image_Post.create({ ...req.body })
+    res.send(post)
+  } catch (error) {
+    throw error
+  }
+}
+
+const UpdatePost = async (req, res) => {
+  try {
+    const post = await Image_Post.update(req.body, {
+      where: { id: req.params.imagepost_id },
+      returning: true
+    })
+    res.send(post)
+  } catch (error) {
+    throw error
+  }
+}
+
+const DeletePost = async (req, res) => {
+  try {
+    await Image_Post.destroy({ where: { id: req.params.imagepost_id } })
+    res.send({
+      msg: 'Post Deleted',
+      payload: req.params.imagepost_id,
+      status: 'Ok'
+    })
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  GetPosts,
+  CreatePost,
+  UpdatePost,
+  DeletePost
+}
---
+++ routes/ImagePostRouter.js
@@ -0,0 +1,25 @@
+const Router = require('express').Router()
+const controller = require('../controllers/ImageController')
+const middleware = require('../middleware')
+
+Router.get('/all', controller.GetPosts)
+Router.post(
+  '/post',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CreatePost
+)
+Router.put(
+  '/:imagepost_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdatePost
+)
+Router.delete(
+  '/:imagepost_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.DeletePost
+)
+
+module.exports = Router
