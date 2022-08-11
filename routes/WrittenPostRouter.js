e09e2e9db1bf3c55d841685565267630fc88e936
Jason Martinez <jasonmar08@gmail.com>
2022-08-11 12:06:29 -0400

    added written_posts routes

--- routes/WrittenPostRouter.js
+++ routes/WrittenPostRouter.js
@@ -1,1 +1,30 @@
 //need model
+const Router = require('express').Router()
+const controller = require('../controllers/UserController.js')
+const middleware = require('../middleware')
+
+Router.get(
+  '/writtenposts',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.GetAllWrittenPosts
+)
+Router.post(
+  '/writtenposts/:user_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CreateWrittenPost
+)
+Router.put(
+  '/writtenposts/:writtenpost_id',
+  middleware.stripToken,
+  middleware.UpdateWrittenPost
+)
+Router.delete(
+  '/writtenposts/:writtenpost_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.DeleteWrittenPost
+)
+
+module.exports = Router
