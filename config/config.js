d553a03d614a53223d60c09da9f40c4713fc143c
Jason Martinez <jasonmar08@gmail.com>
2023-01-15 21:50:52 -0500

    update database config to AWS RDS instance

--- config/config.js
+++ config/config.js
@@ -1,3 +1,33 @@
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    database: process.env.LOCAL_DB_NAME,
+    host: process.env.LOCAL_DB_HOST,
+    dialect: 'postgres'
+  },
+  test: {
+    database: process.env.LOCAL_DB_NAME,
+    host: process.env.LOCAL_DB_HOST,
+    dialect: 'postgres'
+  },
+  production: {
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+        require: true
+      }
+    },
+    ssl: true
+  }
+}
+
 // {
 //   "development": {
 //     "database": "nerds",
@@ -20,24 +50,24 @@
 //   }
 // }
 
-require('dotenv').config()
-module.exports = {
-  development: {
-    database: 'nerds',
-    dialect: 'postgres'
-  },
-  test: {
-    database: 'nerds',
-    dialect: 'postgres'
-  },
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false,
-        require: true
-      }
-    }
-  }
-}
+// require('dotenv').config()
+// module.exports = {
+//   development: {
+//     database: 'nerds',
+//     dialect: 'postgres'
+//   },
+//   test: {
+//     database: 'nerds',
+//     dialect: 'postgres'
+//   },
+//   production: {
+//     use_env_variable: 'DATABASE_URL',
+//     dialect: 'postgres',
+//     dialectOptions: {
+//       ssl: {
+//         rejectUnauthorized: false,
+//         require: true
+//       }
+//     }
+//   }
+// }
